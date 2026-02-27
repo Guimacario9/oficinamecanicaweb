fe8ec0e9946472a54a6539bc5cdf7e57e12751c4
Guilherme Macário <93390112+Guimacario9@users.noreply.github.com>
2026-02-27 10:56:43 -0300

    Add files via upload

---
+++ config/conexao.php
@@ -0,0 +1,15 @@
+﻿<?php
+// config/conexao.php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$banco = "oficinasoft";
+
+$conn = new mysqli($host, $user, $pass, $banco);
+
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
